588179413489128bb2b6fde5df67532447380590
Arbab Kamal <146167220+wingie125@users.noreply.github.com>
2024-07-12 03:11:42 +0530

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = '0c9ae8db-209a-4c91-be6d-798c8ceb7959';
 export const MODAL_CLOSE_SEC = 2.5;
